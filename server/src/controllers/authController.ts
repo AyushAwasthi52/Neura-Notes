2764b933a7d9b665c99e9b38f5c76092df9f4eb9
Ayush Awasthi <ayushawasthi2205@gmail.com>
2026-05-17 13:07:45 +0530

    log routes

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -5,11 +5,12 @@ import { userModel, userSchema } from "../models/userModel.js";
 import { Model, InferSchemaType, HydratedDocument } from "mongoose";
 import catchAsync from "../utils/catchAsync.js";
 import AppError from "../utils/appError.js";
+import { promisify } from "util";
+
+const secretKey: string = process.env.JWT_KEY || "default_fallback_secret";
+const expiresIn: string = process.env.JWT_EXPIRE || "1d";
 
 const signToken: Function = (id: number): string => {
-  const secretKey: string = process.env.JWT_KEY || "default_fallback_secret";
-  const expiresIn: string = process.env.JWT_EXPIRE || "1d";
-
   return jwt.sign({ id }, secretKey, {
     expiresIn: expiresIn as SignOptions["expiresIn"],
   });
@@ -66,5 +67,48 @@ const signup: Function = catchAsync(
       password: req.body.password,
       passwordConfirm: req.body.passwordConfirm,
     });
+  },
+);
+
+const logout: Function = (req: Request, res: Response) => {
+  res.cookie("jwt", "", {
+    expires: new Date(0),
+    httpOnly: true,
+  });
+
+  res.status(200).json({
+    status: "success",
+    message: "Logged Out Successfully",
+  });
+};
+
+const protect: Function = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    let token;
+
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    )
+      token = req.headers.authorization.split(" ")[1];
+    else if (req.cookies.jwt) token = req.cookies.jwt;
+
+    if (!token)
+      return next(new AppError("You are not logged in log in first", 401));
+
+    const decoded = await promisify(jwt.verify as any)(token, secretKey);
+
+    const freshUser = await userModel.findById(decoded.id);
+
+    if (!freshUser)
+      return next(new AppError("The user no longer exists!", 401));
+
+    if (freshUser.changedPasswordAfter(decoded.iat))
+      return next(
+        new AppError(
+          "The user changed password after the toke  was issued",
+          401,
+        ),
+      );
   },
 );
